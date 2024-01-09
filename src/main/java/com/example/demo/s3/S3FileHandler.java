d629f86d5f89d3c1e7ee3231afb5b2b15f29b3c3
Amigo <hyunny4589@nate.com>
2024-01-09 15:44:00 +0900

    added

--- src/main/java/com/example/demo/s3/S3FileHandler.java
+++ src/main/java/com/example/demo/s3/S3FileHandler.java
@@ -1,9 +1,8 @@
 package com.example.demo.s3;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -11,8 +10,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
-import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.UUID;
 
 @Component
 public class S3FileHandler {
@@ -23,7 +25,17 @@ public class S3FileHandler {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
+
     public String uploadFile(MultipartFile multipartFile, String uploadPath) throws IOException {
+        String randomFileName = UUID.randomUUID().toString();
+        ObjectMetadata  objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(multipartFile.getContentType());
+        objectMetadata.setContentLength(multipartFile.getSize());
+        amazonS3.putObject(new PutObjectRequest(bucket,  uploadPath + File.separator + randomFileName, multipartFile.getInputStream(), objectMetadata));
+        return randomFileName;
+    }
+
+    public String uploadFileLocal(MultipartFile multipartFile, String uploadPath) throws IOException {
         String randomFileName = UUID.randomUUID().toString();
         String uploadFileName = uploadPath + File.separator + randomFileName;
         Path localFilePath = Paths.get(System.getProperty("java.io.tmpdir")).resolve(randomFileName);
